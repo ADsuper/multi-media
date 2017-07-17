1c66a9408b397eecc66e2ff7ede7ae31a679de97
ADsuper <rsf411613593@163.com>
2017-07-17 14:46:00 +0800

    MainActivit初始化toolbar

--- app/src/main/java/io/github/adsuper/multi_media/ui/MainActivity.java
+++ app/src/main/java/io/github/adsuper/multi_media/ui/MainActivity.java
@@ -1,15 +1,45 @@
 package io.github.adsuper.multi_media.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.github.adsuper.multi_media.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.viewpager_main)
+    ViewPager viewpagerMain;
+    //底部导航
+    @BindView(R.id.bottom_navigation_view)
+    BottomNavigationView bottomNavigationView;
+    //侧滑菜单
+    @BindView(R.id.navigation_view)
+    NavigationView navigationView;
+    @BindView(R.id.drawerlayout)
+    DrawerLayout drawerlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initToolBar();
+    }
+
+    /**
+     * 初始化 toolbar
+     */
+    private void initToolBar() {
+
+        setSupportActionBar(toolbar);
     }
 }
