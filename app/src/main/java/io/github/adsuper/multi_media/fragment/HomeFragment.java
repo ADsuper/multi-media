a2dc0606f7a0be91fd9f53397f657538ae3cc4b5
ADsuper <rsf411613593@163.com>
2017-07-18 10:47:34 +0800

    22

--- app/src/main/java/io/github/adsuper/multi_media/fragment/HomeFragment.java
+++ app/src/main/java/io/github/adsuper/multi_media/fragment/HomeFragment.java
@@ -45,6 +45,7 @@ public class HomeFragment extends Fragment {
 
     @Override
     public void onAttach(Context context) {
+
         super.onAttach(context);
         mainActivity = (MainActivity) context;
     }
