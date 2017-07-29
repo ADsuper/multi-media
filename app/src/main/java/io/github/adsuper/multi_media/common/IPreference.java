b38b9341cf6bbec28ad72a9a9a5bc5c37a785b3c
ADsuper <rsf411613593@163.com>
2017-07-29 16:27:55 +0800

    SharedPreferences 工具类

---
+++ app/src/main/java/io/github/adsuper/multi_media/common/IPreference.java
@@ -0,0 +1,137 @@
+package io.github.adsuper.multi_media.common;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 操作 SharedPreferences 的工具类接口
+ */
+public interface IPreference {
+
+    /**
+     * 保存一个数据
+     * @param key 键
+     * @param value 值
+     */
+    <T> void put(String key, T value);
+
+    /**
+     * 保存一个Map集合
+     * @param map
+     */
+    <T> void putAll(Map<String, T> map);
+
+    /**
+     * 保存一个List集合
+     * @param key
+     * @param list
+     */
+    void putAll(String key, List<String> list);
+
+    /**
+     * 保存一个List集合，并且自定保存顺序
+     * @param key
+     * @param list
+     * @param comparator
+     */
+    void putAll(String key, List<String> list, Comparator<String> comparator);
+
+    /**
+     * 根据key取出一个数据
+     * @param key 键
+     */
+    <T> T get(String key, DataType type);
+
+    /**
+     * 取出全部数据
+     */
+    Map<String, ?> getAll();
+
+    /**
+     * 取出一个List集合
+     * @param key
+     * @return
+     */
+    List<String> getAll(String key);
+
+    /**
+     * 移除一个数据
+     * @param key
+     * @return
+     */
+    void remove(String key);
+
+    /**
+     * 移除一个集合的数据
+     * @param keys
+     * @return
+     */
+    void removeAll(List<String> keys);
+
+    /**
+     * 移除一个集合的数据
+     * @param keys
+     * @return
+     */
+    void removeAll(String[] keys);
+
+    /**
+     * 是否存在key
+     * @return
+     */
+    boolean contains(String key);
+
+    /**
+     * 清除全部数据
+     */
+    void clear();
+
+    /**
+     * 获取String类型的数据
+     * @param key
+     * @return
+     */
+    String getString(String key);
+
+    /**
+     * 获取Float类型的数据
+     * @param key
+     * @return
+     */
+    float getFloat(String key);
+
+    /**
+     * 获取int类型的数据
+     * @return
+     */
+    int getInteger(String key);
+
+    /**
+     * 获取long类型的数据
+     * @param key
+     * @return
+     */
+    long getLong(String key);
+
+    /**
+     * 获取Set类型的数据
+     * @param key
+     * @return
+     */
+    Set<String> getSet(String key);
+
+    /**
+     * 获取boolean类型的数据
+     * @param key
+     * @return
+     */
+    boolean getBoolean(String key);
+    /**
+     * 枚举：存储或取出的数据类型
+     */
+    enum DataType {
+        INTEGER, LONG, BOOLEAN, FLOAT, STRING, STRING_SET
+    }
+}
--- app/src/main/java/io/github/adsuper/multi_media/ui/SearchActivity.java
+++ app/src/main/java/io/github/adsuper/multi_media/ui/SearchActivity.java
@@ -1,18 +1,85 @@
 package io.github.adsuper.multi_media.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.google.android.flexbox.FlexboxLayout;
+import com.wang.avi.AVLoadingIndicatorView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.github.adsuper.multi_media.R;
+import io.github.adsuper.multi_media.common.PreferenceImpl;
+import io.github.adsuper.multi_media.widget.EmptyRecyclerView;
 
 /**
  * 搜索页面
  */
 public class SearchActivity extends AppCompatActivity {
 
+    @BindView(R.id.iv_back)
+    ImageView mIvBack;
+    @BindView(R.id.tv_search)
+    TextView mTvSearch;
+    @BindView(R.id.et_search)
+    EditText mEtSearch;
+    @BindView(R.id.flexbox_layout)
+    FlexboxLayout mFlexboxLayout;
+    @BindView(R.id.iv_deleteall)
+    ImageView mIvDeleteall;
+    @BindView(R.id.layout_history)
+    RelativeLayout mLayoutHistory;
+    @BindView(R.id.avi)
+    AVLoadingIndicatorView mAvi;
+    @BindView(R.id.avi_loadmore)
+    AVLoadingIndicatorView mAviLoadmore;
+    @BindView(R.id.layout_loadmore)
+    LinearLayout mLayoutLoadmore;
+    @BindView(R.id.recyclerview_history)
+    EmptyRecyclerView mRecyclerviewHistory;
+    @BindView(R.id.activity_search)
+    LinearLayout mActivitySearch;
+    /**
+     * SharedPreferences 存储的历史搜索记录文件夹名称
+     */
+    private final String HISTORY_SEARCH = "history_search";
+
+    private Context mContext;
+    // sp 操作的工具类
+    private PreferenceImpl mPreference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+        mContext = this;
+
+        initSP();
+    }
+
+    private void initSP() {
+        mPreference = (PreferenceImpl) PreferenceImpl.getPreference(this, HISTORY_SEARCH);
+    }
+
+
+    @OnClick({R.id.iv_back, R.id.et_search, R.id.iv_deleteall})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.iv_back://返回图标的点击事件
+                break;
+            case R.id.et_search://搜索按钮点击事件
+                break;
+            case R.id.iv_deleteall://历史记录删除点击事件
+                break;
+        }
     }
 }
